840b7adf9124f1e1f63a2635aea0d0b8fb320873
Shinwoo Kim <shinwookim@pitt.edu>
2022-12-09 12:30:49 -0500

    WF

--- .github/build.sh
+++ .github/build.sh
@@ -1,9 +1,9 @@
-SRC="../../src/*.md"
-rm -rf ../../out
+SRC="./src/*.md"
+rm -rf ./out
 mkdir out
 for s in $SRC; 
 do
     echo "$s"
-    marp --theme ./src/theme/pitt.css $s --html -o ../../out/$(basename "$s" | sed 's/\(.*\)\..*/\1/').html
+    marp --theme ./src/theme/pitt.css $s --html -o ./out/$(basename "$s" | sed 's/\(.*\)\..*/\1/').html
 done
 
